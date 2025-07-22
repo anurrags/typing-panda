1d2a233c89722126498347d16e0a1994ffbfc9f1
Anurag Sharma <iamanurag0501@gmail.com>
2025-07-23 02:03:39 +0530

    add zustand to manage global stage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,44 @@
+"use client";
+
+import { useTabStore } from "@/store";
 import Image from "next/image";
 import React from "react";
 
 const Header: React.FC = () => {
+  const { tab, setTab } = useTabStore((state) => state);
   return (
     <div className="flex justify-between items-center py-4 px-12">
-      <div className="flex items-center gap-8">
-        <div className="flex items-center gap-2">
+      <div className="flex items-center gap-12">
+        <div className="flex items-center gap-1">
           <Image src="/panda.svg" alt="icon" width={32} height={32} />
           <h1 className="text-2xl font-bold">Typing Panda</h1>
         </div>
         <div>
-          <ul className="flex items-center gap-4">
-            <li>Practice</li>
-            <li>Leaderboard</li>
-            <li>About</li>
+          <ul className="flex items-center text-xl gap-4">
+            <li
+              onClick={() => setTab("practice")}
+              className={`${
+                tab === "practice" && "text-cyan-1"
+              } cursor-pointer hover:text-cyan-2`}
+            >
+              Practice
+            </li>
+            <li
+              onClick={() => setTab("leaderboard")}
+              className={`${
+                tab === "leaderboard" && "text-cyan-1"
+              } cursor-pointer hover:text-cyan-2`}
+            >
+              Leaderboard
+            </li>
+            <li
+              className={`${
+                tab === "about" && "text-cyan-1"
+              } cursor-pointer hover:text-cyan-2`}
+              onClick={() => setTab("about")}
+            >
+              About
+            </li>
           </ul>
         </div>
       </div>
