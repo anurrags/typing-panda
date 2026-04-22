60e1f1f06af3f1e78be815fc03287042ed28787b
Anurag Sharma <iamanurag0501@gmail.com>
2026-04-22 10:20:01 +0530

    feat: add password recovery flow with email request and password reset page

---
+++ src/app/(base)/reset-password/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ResetPassword } from "@/components";
+
+const Page = () => {
+  return (
+    <div className="fade-slide-down flex min-h-[85vh] flex-col items-center justify-center">
+      <ResetPassword />
+    </div>
+  );
+};
+
+export default Page;
